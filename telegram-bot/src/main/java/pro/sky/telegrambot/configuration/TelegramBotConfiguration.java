977b72f7c9a4d305c62e6d6e77d513e2626d5a36
warlocktony <antonz3594@gmail.com>
2023-10-09 21:24:31 +0300

    ready

--- telegram-bot/src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
+++ telegram-bot/src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
@@ -18,5 +18,4 @@ public class TelegramBotConfiguration {
         bot.execute(new DeleteMyCommands());
         return bot;
     }
-
 }
---
+++ telegram-bot/src/main/java/pro/sky/telegrambot/service/TgBotService.java
@@ -0,0 +1,9 @@
+package pro.sky.telegrambot.service;
+
+import com.pengrad.telegrambot.model.Message;
+
+public interface TgBotService {
+
+    void addTask(Message message);
+
+}
